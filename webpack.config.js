74402f6fc8a507d54aff37ba7f3008ddd5b98c1c
leejss <goldemshine@gmail.com>
2022-11-03 19:49:59 +0900

    runtime

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,84 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const WebpackBar = require("webpackbar");
-const webpack = require("webpack");
-const { merge } = require("webpack-merge");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const WebpackBar = require('webpackbar')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const commonConfig = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "build"),
-    },
-    stats: {
-        errorDetails: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [{ loader: "babel-loader" }],
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-
-    plugins: [
-        new WebpackBar(),
-        new webpack.ProvidePlugin({
-            React: "react",
-        }),
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-            filename: "index.html",
-            title: "Webpack Setup",
-        }),
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  stats: {
+    errorDetails: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-};
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  plugins: [
+    new WebpackBar(),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      title: 'Webpack Setup',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'source',
+          to: 'desc',
+        },
+      ],
+    }),
+  ],
+}
 
 /** @type {import('webpack').Configuration} */
 const devConfig = {
-    mode: "development",
-    devtool: "cheap-module-source-map",
-    devServer: {
-        hot: true,
-    },
-    plugins: [new ReactRefreshWebpackPlugin()],
-};
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+  },
+  plugins: [new ReactRefreshWebpackPlugin()],
+}
 
 /** @type {import('webpack').Configuration} */
 const prodConfig = {
-    mode: "production",
-    devtool: "source-map",
-};
+  mode: 'production',
+  devtool: 'source-map',
+}
 
 module.exports = ({ env }) => {
-    return env === "dev" ? merge(commonConfig, devConfig) : merge(commonConfig, prodConfig);
-};
+  return env === 'dev'
+    ? merge(commonConfig, devConfig)
+    : merge(commonConfig, prodConfig)
+}

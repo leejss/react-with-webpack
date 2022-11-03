df58dd293ba78a517c0ab92804c7cb8b6b1b75aa
leejss <goldemshine@gmail.com>
2022-11-03 19:03:40 +0900

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const App = () => {
-    const [count, setCount] = useState(0);
-    return (
-        <h1>
-            hell1232313231o sds! {process.env.NODE_ENV}
-            <button
-                onClick={() => {
-                    setCount((prev) => prev + 1);
-                }}
-            >
-                inc {count}
-            </button>
-        </h1>
-    );
-};
+  const [count, setCount] = useState(0)
+  return (
+    <h1>
+      hell1232313231o sds! {process.env.NODE_ENV}
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1)
+        }}
+      >
+        inc {count}
+      </button>
+    </h1>
+  )
+}
 
-export default App;
+export default App
